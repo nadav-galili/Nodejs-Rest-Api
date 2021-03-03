38baede4ac9376c66f0b03a49f8eeefe87573a3c
nadav-galili <67638018+nadav-galili@users.noreply.github.com>
2021-03-03 16:11:55 +0200

    added get cards endpoint

--- middleware/auth.js
+++ middleware/auth.js
@@ -1,19 +1,18 @@
-const jwt=require('jsonwebtoken');
-const config=require('config');
+const jwt = require("jsonwebtoken");
+const config = require("config");
 
-module.exports=(req,res,next)=>{
+module.exports = (req, res, next) => {
+  const token = req.header("x-auth-token");
+  // check if theres a token
+  if (!token) return res.status(401).send("Access denied, No token provided");
 
-const token=req.header('x-auth-token');
-// check if theres a token
-if(!token) return res.status(401).send('Access denied, No token provided');
-
-// if the token is ok then...
-try {
-const decoded=jwt.verify(token,config.get('jwtKey'));
-req.user=decoded;
-next();
-// if the token is not ok
-} catch(ex){
-    res.status(400).send('Invalid token')
-}
+  // if the token is ok then...
+  try {
+    const decoded = jwt.verify(token, config.get("jwtKey"));
+    req.user = decoded;
+    next();
+    // if the token is not ok
+  } catch (ex) {
+    res.status(400).send("Invalid token");
+  }
 };
--- routes/cards.js
+++ routes/cards.js
@@ -1,59 +1,76 @@
-const express=require ('express');
-const _ = require('lodash');
-const {Card,validateCard,generateRandomNumber}=require ('../models/card');
-const auth=require('../middleware/auth');
-const router=express.Router();
+const express = require("express");
+const _ = require("lodash");
+const { Card, validateCard, generateRandomNumber } = require("../models/card");
+const auth = require("../middleware/auth");
+const { Router } = require("express");
+const router = express.Router();
 
+router.get("/my-cards", auth, async (req, res) => {
+  if (!req.user.biz) return res.status(401).send("Access denied");
 
-router.delete('/:id',auth, async(req,res)=>{
-    const card=await Card.findOneAndRemove({_id:req.params.id,user_id:req.user._id});
-    if(! card) return res.status(404).send('The cards with the given ID not found.');
-    res.send(card);
-})
-
-
-
-router.put('/:id',auth,async(req,res)=>{
-    const {error}=validateCard(req.body);
-    if(error) return res.status(400).send(error.details[0].message);
-
-    let card=await Card.findOneAndUpdate({_id:req.params.id,user_id:req.user._id},req.body);
-    if(!card) return res.status(404).send('The cards with the given ID not found.');
-
-card=await Card.findOne({_id:req.params.id,user_id:req.user._id});
-res.send(card)
-
+  const cards = await Card.find({ user_id: req.user._id });
+  res.send(cards);
 });
 
-router.get('/:id',auth,async (req,res)=>{
-
-    const card=await Card.findOne({_id:req.params.id,user_id:req.user._id});
-    if(!card) return res.status(404).send('The cards with the given ID not found.');
-    res.send(card);
-})
-
-router.post('/', auth, async(req,res)=>{
-    // this is a method to check if a random no is made
-// const randomNumber=await generateRandomNumber(Card);
-// res.send(randomNumber);
-
-const {error}=validateCard(req.body);
-if(error) return res.status(400).send(error.details[0].message);
-
-let card=new Card({
-    bizName:req.body.bizName,
-    bizDescription:req.body.bizDescription,
-    bizAddress:req.body.bizAddress,
-    bizPhone:req.body.bizPhone,
-    bizImage:req.body.bizImage? req.body.bizImage:'https://cdn.pixabay.com/photo/2015/10/30/10/03/app-1013616_960_720.jpg',
-    bizNumber:await generateRandomNumber(Card),
-    user_id:req.user._id
+// endpoint for deleting cards
+router.delete("/:id", auth, async (req, res) => {
+  const card = await Card.findOneAndRemove({
+    _id: req.params.id,
+    user_id: req.user._id,
+  });
+  if (!card)
+    return res.status(404).send("The cards with the given ID not found.");
+  res.send(card);
 });
 
-let post=await card.save();
-res.send(post);
+// endpoint for editing cards
+router.put("/:id", auth, async (req, res) => {
+  const { error } = validateCard(req.body);
+  if (error) return res.status(400).send(error.details[0].message);
+
+  let card = await Card.findOneAndUpdate(
+    { _id: req.params.id, user_id: req.user._id },
+    req.body
+  );
+  if (!card)
+    return res.status(404).send("The cards with the given ID not found.");
+
+  card = await Card.findOne({ _id: req.params.id, user_id: req.user._id });
+  res.send(card);
 });
 
+router.get("/:id", auth, async (req, res) => {
+  const card = await Card.findOne({
+    _id: req.params.id,
+    user_id: req.user._id,
+  });
+  if (!card)
+    return res.status(404).send("The cards with the given ID not found.");
+  res.send(card);
+});
 
-module.exports=router;
+router.post("/", auth, async (req, res) => {
+  // this is a method to check if a random no is made
+  // const randomNumber=await generateRandomNumber(Card);
+  // res.send(randomNumber);
 
+  const { error } = validateCard(req.body);
+  if (error) return res.status(400).send(error.details[0].message);
+
+  let card = new Card({
+    bizName: req.body.bizName,
+    bizDescription: req.body.bizDescription,
+    bizAddress: req.body.bizAddress,
+    bizPhone: req.body.bizPhone,
+    bizImage: req.body.bizImage
+      ? req.body.bizImage
+      : "https://cdn.pixabay.com/photo/2015/10/30/10/03/app-1013616_960_720.jpg",
+    bizNumber: await generateRandomNumber(Card),
+    user_id: req.user._id,
+  });
+
+  let post = await card.save();
+  res.send(post);
+});
+
+module.exports = router;
